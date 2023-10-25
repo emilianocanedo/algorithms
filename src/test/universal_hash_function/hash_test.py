5f27fb24adb2ca36ebe6ce3f5a482521f861792a
Emiliano Canedo <emilianocanedo@gmail.com>
2023-10-25 15:23:52 +0200

    A1: Adding range, integrity and uniqueness tests for Hash class

--- src/test/universal_hash_function/hash_test.py
+++ src/test/universal_hash_function/hash_test.py
@@ -1,9 +1,25 @@
-import pytest
-
 from src.universal_hash_function.hash import Hash
+from random import sample
 
 def test_range():
     myhash = Hash(300)
-    assert myhash.hash(6656504) < 300
-    assert myhash.hash(4349854) < 300
-    assert myhash.hash(543212) < 300
+    randlist = sample(range(0, pow(2, 16)), 600)
+
+    for num in randlist:
+        assert 0 <= myhash.hash(num) < 300
+
+def test_integrity():
+    myhash = Hash(300)
+    randlist = sample(range(0, pow(2, 16)), 100)
+
+    for num in randlist:
+        call1 = myhash.hash(num)
+        call2 = myhash.hash(num)
+        assert call1 == call2
+
+def test_uniqueness():
+    myhash = Hash(5000)
+    randlist = sample(range(0, pow(2, 16)), 100)
+
+    randset = set(randlist)
+    assert len(randset) == 100
